597afc535de398245d9d6a8dd3fa869f234f391d
Zack Mully <zack@925labs.com>
2016-04-11 17:53:20 -0400

    Added rule deletion

--- bin/lambda-cfn-rules.js
+++ bin/lambda-cfn-rules.js
@@ -3,49 +3,205 @@
 var fs = require('fs');
 var path = require('path');
 var AWS = require('aws-sdk');
-var queue = require('queue-async');
+var d3 = require('d3-queue');
+var inquirer = require('inquirer');
+var argv = require('yargs')
+      .usage('Usage: $0 <command> [options]')
+      .demand(1)
+      .command('create <stack>','creates CloudWatch Event Rules for a Patrol stack')
+      .command('list <prefix>','list CloudWatch Event Rules matching the given prefix string')
+      .command('delete <prefix>','deletes CloudWatch Event Rules matching the given prefix string')
+      .argv;
 
-if (!process.argv[2])
-  throw new Error('Must provide name of CloudFormation stack as first argument');
+var lambdaCfnRules = module.exports;
+lambdaCfnRules.getMatchedRules = getMatchedRules;
+lambdaCfnRules.deleteRuleset = deleteRuleset;
+lambdaCfnRules.removeTargets = removeTargets;
+lambdaCfnRules.argv = argv;
 
 var rules = [];
-var files = fs.readdirSync(path.join(process.cwd(), 'cloudformation'));
+var matchedRules = [];
+lambdaCfnRules.matchedRules = matchedRules;
+
+
+
+if (process.env.NODE_ENV == 'test') {
+  var files = fs.readdirSync(path.join(process.cwd(), 'test/cloudformation'));
+} else {
+  var files = fs.readdirSync(path.join(process.cwd(), 'cloudformation'));
+}
 
 for (var i = 0; i < files.length; i++) {
   var file = files[i];
   if (path.extname(file) == '.js' && file.indexOf('.template.') > -1) {
-    templateFile = path.join(process.cwd(), 'cloudformation', file);
+    if (process.env.NODE_ENV == 'test') {
+      templateFile = path.join(process.cwd(), 'test/cloudformation/lambda-rules.template.js');
+    } else {
+      templateFile = path.join(process.cwd(), 'cloudformation', file);
+    }
     break;
   }
 }
 
 var template = require(templateFile);
+
 for (var r in template.Resources) {
   if (template.Resources[r].Type == 'AWS::Lambda::Function' &&
       template.Resources[r].Metadata &&
       template.Resources[r].Metadata.sourcePath) {
     var sourcePath = path.join(process.cwd(), template.Resources[r].Metadata.sourcePath);
     var rule = require(sourcePath);
+    rule.config.stackRuleName = argv.stack + '-' + rule.config.name;
     rules.push(rule);
   }
 }
 
-var stackName = process.argv[2];
+var stackName = argv.stack;
 var region = process.env.AWS_DEFAULT_REGION || 'us-east-1';
-var q = queue(1);
+var q = d3.queue(1);
 var cfn = new AWS.CloudFormation({region: region});
 var cwe = new AWS.CloudWatchEvents({region: region});
 var lambda = new AWS.Lambda({region: region});
 
-q.defer(getStackResources);
-q.defer(createEventRules);
-q.awaitAll(function(err) {
-  if (err) throw err;
-  else console.log('CloudWatch Event Rules created');
-});
+if (argv._ == 'create') {
+  q.defer(getStackResources);
+  q.defer(createEventRules);
+  q.awaitAll(function(err) {
+    if (err) throw err;
+    else console.log('CloudWatch Event Rules creation complete');
+  });
+}
+
+if (argv._ == 'delete') {
+  q.defer(getMatchedRules);
+  q.defer(listRules);
+  q.defer(deleteRules);
+  q.awaitAll(function(err) {
+    if (err) throw err;
+    else console.log('CloudWatch Event Rules deletion complete');
+  });
+}
+
+if (argv._ == 'list') {
+  q.defer(getMatchedRules);
+  q.defer(listRules);
+  q.awaitAll(function(err) {
+    if (err) throw err;
+  });
+}
+
+function deleteRules(callback) {
+  inquirer.prompt([
+    {
+      type: 'confirm',
+      message: 'Please confirm the deletion of the CloudWatch rules',
+      name: 'delete',
+      default: false
+    }
+  ]).then(function(answers) {
+    if (answers.delete == true) {
+      deleteRuleset(matchedRules, function(err){
+        if (err) return callback(err);
+        return callback();
+      });
+    } else {
+      callback();
+    }
+  });
+}
+
+function listRules(callback) {
+  if (matchedRules.length > 0) {
+    matchedRules.forEach(function(rule) {
+      console.log(rule.config.stackRuleName);
+    });
+    callback();
+  } else {
+    callback(new Error('No matching rules found'));
+  }
+};
+
+function deleteRuleset(ruleset, callback) {
+  var q = d3.queue(1);
+  q.defer(removeTargets,ruleset);
+  ruleset.forEach(function(rule) {
+    var deleteParams = { Name: rule.config.stackRuleName };
+    q.defer(function(next) {
+      cwe.deleteRule(deleteParams, function(err,res) {
+        if (err && err.code == 'ValidationException') {
+          console.log('ERROR: ' + rule.config.stackRuleName + ' still has targets, skipping..');
+          return next();
+        }
+        if (err) return next(err);
+        console.log('DELETED: '+ rule.config.stackRuleName);
+        next();
+      });
+    });
+  });
+  q.awaitAll(function(err) {
+    callback(err);
+  });
+};
+
+function removeTargets(ruleset, callback) {
+  var q = d3.queue(1);
+  ruleset.forEach(function(rule) {
+    var listParams = { Rule: rule.config.stackRuleName };
+    q.defer(function(next) {
+      cwe.listTargetsByRule(listParams, function(err,res) {
+        if (err) return next(err);
+        if (res.Targets.length > 0) {
+          var targets = [];
+          res.Targets.forEach(function(target) {
+            targets.push(target.Id);
+          });
+          var removeTargets = { Rule: rule.config.stackRuleName, Ids: targets};
+          cwe.removeTargets(removeTargets, function(err,res) {
+            if (err) return next(err);
+            next();
+          });
+        } else {
+          next();
+        }
+      });
+    });
+  });
+  q.awaitAll(function(err) {
+    callback(err);
+  });
+};
+
+function getMatchedRules(callback) {
+  var q = d3.queue(1);
+  var listParams = { NamePrefix: argv.prefix };
+  function findRules(params, next) {
+    cwe.listRules(params, function(err,res) {
+      if (err) return next(err);
+      res.Rules.forEach(function(rule) {
+        var rname = {
+          config:
+          {
+            stackRuleName: rule.Name
+          }
+        };
+        matchedRules.push(rname);
+      });
+      if (res.NextToken) {
+        listParams.NextToken = res.NextToken;
+        findRules(listParams,next);
+      } else {
+        next();
+      }
+    });
+  };
+  q.defer(findRules,listParams);
+  q.awaitAll(function(err) {
+    callback(err);
+  });
+};
 
 function createEventRules(callback) {
-  var q = queue();
+  var q = d3.queue();
   rules.forEach(function(rule) {
     var name = stackName + '-' + rule.config.name;
     if (rule.config.eventRule) {
@@ -72,6 +228,7 @@ function createEventRules(callback) {
           });
         });
       });
+      console.log('CREATED: ' + eName);
     }
     if (rule.config.scheduledRule) {
       var sName = name + '-scheduled';
@@ -97,16 +254,17 @@ function createEventRules(callback) {
           });
         });
       });
+      console.log('CREATED: ' + sName);
     }
   });
 
   q.awaitAll(function(err) {
-  callback(err);
+    callback(err);
   });
 }
 
 function getStackResources(callback) {
-  var q = queue();
+  var q = d3.queue();
 
   cfn.describeStackResources({StackName: stackName}, function(err, data) {
     if (err) throw err;

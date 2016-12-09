6773bc9c74c50fafaf7c6ec20d82e3ea9e031adf
Paul Lucas <lucaspa@umich.edu>
2016-12-09 10:01:32 -0800

    Modifing templates to match new env layout
    In lambda-cfn reverting the removal of the ref parsing
    for the envVariableParser. This helped make the unit
    tests align better with the expected output
    
    Updating all of the templates to match the
    aws lambda sytle env variable style with
    Environment.Variables object.

--- test/build.test.js
+++ test/build.test.js
@@ -48,13 +48,6 @@ tape('Compile unit tests', function(t) {
   var fullBuilt = lambdaCfn.compile([lambdaCfn.build(fullConfig)], {});
   var fullFixture = JSON.parse(fs.readFileSync(path.join(__dirname, './fixtures/full.template'), "utf8"));
 
-    console.log("Fixture-------------------");
-    console.log(util.inspect(fullFixture, false, null));
-    console.log("Fixture-------------------");
-    console.log("Built-------------------");
-    console.log(util.inspect(fullBuilt, false, null));
-    console.log("Built-------------------");
-
     t.deepEqual(fullBuilt, fullFixture, 'full build is equal to fixture');
 
   t.end();
@@ -117,7 +110,7 @@ tape('Compile Event rule', function(t) {
   var eventBuilt = lambdaCfn.compile([lambdaCfn.build(eventConfig)], {});
   var eventFixture = JSON.parse(fs.readFileSync(path.join(__dirname, './fixtures/event.template'), "utf8"));
 
-  t.deepEqual(eventBuilt,eventFixture, 'Event rule build is equal to fixture');
+    t.deepEqual(eventBuilt,eventFixture, 'Event rule build is equal to fixture');
 
   t.end();
 });

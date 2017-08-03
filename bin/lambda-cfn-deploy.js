15e0f3e8bcf554f8b67774838c8defd74ec5d27b
Alexandra Ulsh <ulsh@mapbox.com>
2017-08-03 15:24:43 -0700

    Moves parameter checking logic from bin to lib for easier testing

--- bin/lambda-cfn-deploy.js
+++ bin/lambda-cfn-deploy.js
@@ -15,34 +15,4 @@ if (!argv._[3]) {
 var command = argv._[2];
 var environment = argv._[3];
 
-if (argv.name) {
-  var stackName = argv.name;
-} else {
-  var stackName = path.basename(process.cwd());
-}
-
-if (argv.template) {
-  var template = argv.template;
-} else {
-  var template = path.join(process.cwd(), 'function.template.js');
-}
-
-if (argv.cfnConfigbucket) {
-  var cfnConfigBucket = argv.cfnConfigbucket;
-} else {
-  var cfnConfigBucket = process.env.CFN_CONFIG_BUCKET;
-}
-
-if (argv.region) {
-  var region = argv.region;
-} else {
-  var region = process.env.AWS_DEFAULT_REGION;
-}
-
-if (argv.templateBucket) {
-  var templateBucket = argv.templateBucket;
-} else {
-  var templateBucket = 'cfn-config-templates-' + process.env.AWS_ACCOUNT_ID + '-' + region;
-}
-
-lambdaCfnDeploy.deploy(command, stackName, environment, template, region, cfnConfigBucket, templateBucket);
+lambdaCfnDeploy.deploy(command, environment, argv.name, argv.template, argv.region, argv.cfnConfigbucket, argv.templateBucket);
